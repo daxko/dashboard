e34ce709e364edb811e60bb0f5190d14c0026801
Adam Aldrich <adam@adamaldrich.com>
2012-06-04 17:48:14 -0500

    Added a page to see if Operations is up or down

---
+++ web/Models/ApplicationStatusIndicatorViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace web.Models
+{
+    public class ApplicationStatusIndicatorViewModel
+    {
+        public bool status { get; set; }
+        public string name { get; set; }
+    }
+}
---
+++ web/Models/ShowIfOpsIsUpViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace web.Models
+{
+    public class ShowIfOpsIsUpViewModel
+    {
+        public ApplicationStatusIndicatorViewModel ops_1 { get; set; }
+        public ApplicationStatusIndicatorViewModel ops_2 { get; set; }
+        public ApplicationStatusIndicatorViewModel ops_3 { get; set; }
+    }
+}
