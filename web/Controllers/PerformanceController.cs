2d6229f423b9764e617ae6342df7ae91e711759e
Adam Aldrich <adam@adamaldrich.com>
2012-06-04 00:09:57 -0500

    Added a Performance section with the 20 longest running pages

---
+++ web/Controllers/PerformanceController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web.Controllers
+{
+    public class PerformanceController : Controller
+    {
+        public ActionResult LongestRunningPages()
+        {
+            return View();
+        }
+    }
+}
