2af304cf8afdf7863133fcfe7124b2e2ed2b4823
Austin Aldrich <austin4oghs@gmail.com>
2012-06-28 08:02:57 -0500

    Added some files I forgot to commit.

---
+++ web/Models/DatabaseMetrics/SqlJobMetricDashboardViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace web.Models.DatabaseMetrics
+{
+    public class SqlJobMetricDashboardViewModel
+    {
+        public IEnumerable<SqlJobMetricViewModel> failed_jobs { get; set; }
+        public int failed_jobs_count { get; set; }
+        public IEnumerable<SqlJobMetricViewModel> successful_jobs { get; set; }
+        public int successful_jobs_count { get; set; }
+        public int total_jobs_count { get; set; }
+    }
+}
---
+++ web/Models/DatabaseMetrics/SqlJobMetricViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace web.Models.DatabaseMetrics
+{
+    public class SqlJobMetricViewModel
+    {
+        public string job_name { get; set; }
+        public string message { get; set; }
+        public string last_run { get; set; }
+    }
+}
