1380857b6f7c475321be0ad6095278b4673ebd76
Austin Aldrich <austin4oghs@gmail.com>
2012-06-29 13:20:02 -0500

    Updated sql metrics repo to use ProductionSupport cache db.

--- domain/DatabaseMetrics/SqlJobMetricRepository.cs
+++ domain/DatabaseMetrics/SqlJobMetricRepository.cs
@@ -22,19 +22,7 @@ namespace domain.DatabaseMetrics
             var results = new List<SqlJobMetrics>();
 
             const string sql =
-                @"select j.name,
-                   h.message,
-                   success_status = case h.run_status
-                                        when 1 then 1 /* success */
-                                        else 0 /* failure */
-                                    end,
-                    last_run_datetime = CONVERT(DATETIME, CONVERT(CHAR(8), run_date, 112) + ' ' + STUFF(STUFF(RIGHT('000000' + CONVERT(VARCHAR(8), run_time), 6), 5, 0, ':'), 3, 0, ':'), 121),
-                    h.step_id
-                from msdb.dbo.sysjobhistory h
-                inner join msdb..sysjobs j on h.job_id = j.job_id
-                where CONVERT(DATETIME, CONVERT(CHAR(8), run_date, 112) + ' ' + STUFF(STUFF(RIGHT('000000' + CONVERT(VARCHAR(8), run_time), 6), 5, 0, ':'), 3, 0, ':'), 121) 
-                    > DATEADD(HOUR, -24, GETDATE())
-                ";
+                @"select * from ProductionSupport.dbo.Dashboard_SqlJobMetrics_Keep";
 
             var select_cmd = new SqlCommand(sql);
             select_cmd.Connection = (SqlConnection) db_connection;
@@ -61,11 +49,11 @@ namespace domain.DatabaseMetrics
             metric.job_name = reader.GetString(0);
             metric.message = reader.GetString(1);
 
-            var job_outcome_key = reader.GetInt32(2);
+            var job_outcome_key = reader.GetByte(2);
             metric.job_status = get_job_outcome_from_key(job_outcome_key);
 
             metric.last_run = reader.GetDateTime(3);
-            metric.job_step = reader.GetInt32(4);
+            metric.job_step = reader.GetInt16(4);
 
             return metric;
         }
