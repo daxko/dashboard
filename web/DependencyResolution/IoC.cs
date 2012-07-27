aa7d9953076923eef57a567b494f5e0d6b1995b0
Austin Aldrich <aualdrich@gmail.com>
2012-07-27 17:16:22 -0500

    Added proper encryption to the connection string so the app is now compliant for production.

--- web/DependencyResolution/IoC.cs
+++ web/DependencyResolution/IoC.cs
@@ -1,6 +1,10 @@
+using System;
+using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.IO;
+using Daxko.Infrastructure.DataAccess;
+using Daxko.Infrastructure.Security;
 using domain.DatabaseMetrics;
 using StructureMap;
 using domain.Quotes;
@@ -25,10 +29,19 @@ namespace web {
         static void wire_up_database(IInitializationExpression r)
         {
 
-            //todo:  refactor this to use proper security encryption when retrieving a connection string
-            string db_connection_path = @"C:\\codebase\\Dashboard\\db_connection_path.txt";
+            var encrypted_connection_strings = ConfigurationManager.ConnectionStrings;
 
-            r.For<IDbConnection>().Use(x => new SqlConnection(File.OpenText(db_connection_path).ReadToEnd()));
+            r.ForSingletonOf<ICrypto>().Use<Crypto>()
+                .Ctor<String>("masterCipher").EqualToAppSetting("PassPhrase");
+
+            r.ForSingletonOf<IConnectionStringManager>()
+              .Use<ConnectionStringManager>()
+              .Ctor<ConnectionStringSettingsCollection>("encryptedConnections").Is(encrypted_connection_strings)
+              .Ctor<bool>("ignoreLocalSqlServer").Is(true);
+
+            r.For<IDbConnection>().Use<SqlConnection>()
+                .Ctor<string>("connectionString").Is(
+                    x => x.GetInstance<IConnectionStringManager>().Find("dashboard_connection_string").ConnectionString);
 
         }
 
